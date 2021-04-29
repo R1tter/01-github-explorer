3f98b652d66502647aaaca750fb62fc953202c9c
Marcelo Ritter <marcelo.ritter@dotgroup.com.br>
2021-04-29 14:28:06 -0300

    feature: first component and scss

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,36 @@
+const repositoryName = 'unform';
+
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+            </ul>
+        </section>
+    )
+}
