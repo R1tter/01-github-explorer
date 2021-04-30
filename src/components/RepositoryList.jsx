2c119ebf176027f7b313da520d71d9b83df78d76
Marcelo Ritter <marcelo.ritter@dotgroup.com.br>
2021-04-30 13:55:47 -0300

    Feature: Consuming git api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -4,13 +4,6 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 
 
-const repository = {
-  name: 'R1tter',
-  description: 'Academical applications',
-  link: 'https://github.com/R1tter'
-
-}
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
@@ -19,18 +12,15 @@ export function RepositoryList() {
       .then(response => response.json())
       .then(data => setRepositories(data))
   }, []);
-  
-  console.log(repositories);
 
   return (
     <section className="repository-list">
       <h1>Ritter's Profile</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   );
